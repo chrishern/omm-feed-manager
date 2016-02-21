ac5307dc02976c57e4da2298d2510cb5c4aa5495
Chris Hern <chrishern@hotmail.com>
2016-02-21 19:48:37 +0000

    Added comment explaining the aim of the component test.

--- omm-feed-manager-app/src/test/java/net/chris/messaging/IncomingMessageListenerComponentTest.java
+++ omm-feed-manager-app/src/test/java/net/chris/messaging/IncomingMessageListenerComponentTest.java
@@ -15,6 +15,14 @@ import net.chris.caerus.output.CaerusOutput;
 import net.chris.incident.IncidentProcessor;
 import net.chris.test.common.ActiveMqPublishSubscribeTest;
 
+/**
+ * Component test for the {@link IncomingMessageListener}.  Its aim is to make sure that the component is 
+ * able to successfully read messages posted to the appropriate ActiveMQ topic and then pass them onto the 
+ * interested component.  Any testing of internal logic will be done in a standalone unit test.
+ * 
+ * @author chris
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(MessageListenerComponentTestConfig.class)
 public class IncomingMessageListenerComponentTest extends ActiveMqPublishSubscribeTest {
